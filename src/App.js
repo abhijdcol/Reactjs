1a94bd97434bec43d86ac2dba794cbe4d66673ec
abhijit jamdade <abhijd440@gmail.com>
2023-07-28 19:43:07 +0530

    First project

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React", isCompleted: false },
+    { text: "Meet friend for lunch", isCompleted: false },
+    { text: "Build really cool todo app", isCompleted: false },
+  ]);
+
+  const addTodo = (text) => {
+    const newTdos = [...todos, { text }];
+    setTodos(newTdos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+  return (
+    <div className="App">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
